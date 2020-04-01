48cc588ee5ab206be01528137575bf8735d21d16
Opjot Singh Nijjar <opjot-singh-nijjar@Opjots-MacBook-Pro.local>
2020-04-01 15:31:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$('.menu').click(function(){
+    $(".icon").toggleClass('close')
+    
+    $(".active").removeClass("active")
+
+    // $("nav").addClass("active");
+})
+
+// $('.active').toggle
+
+// $('').toggle(function () {
+//   $(".active").addClass("active");
+// }, function () {
+//   $(".active").removeClass("active");
+// });
